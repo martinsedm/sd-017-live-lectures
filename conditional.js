509933eaac5dd7d26cb26a1b13aa56c2b23adf2f
Henrique Jensen <henriquejensen@hotmail.com>
2021-10-08 15:28:06 -0300

    ajuste do codigo

--- conditional.js
+++ conditional.js
@@ -1,19 +1,16 @@
 let num1 = 5;
 let num2 = 10;
 let num3 = 15;
+let age = 18;
 
-if (num1 > num2) {
-  console.log("num1 wins");
+if (age == 18) {
+  console.log("você pode dirigir");
 } else {
-  console.log("num2 wins")
+  console.log("desculpa, você não pode dirigir")
 }
 
 // Como saber se o num1 + num 2 é par?
 let isEven = ((num1 + num2) % 2) == 0
-console.log(isEven);
-
-// E o num1 + num2 + num3?
-isEven = ((num1 + num2 + num3) % 2) == 0
 console.log(isEven);
 
 // 1 é igual a "1"
