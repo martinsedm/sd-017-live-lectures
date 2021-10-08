41a73ba2a45fcd35ec9e1147071faf8e5689db32
Henrique Jensen <henriquejensen@hotmail.com>
2021-10-08 16:18:03 -0300

    ajustes

--- arithmetic.js
+++ arithmetic.js
@@ -4,15 +4,20 @@ let num3 = 15;
 
 // Soma
 let soma = num1 + num2;
+console.log(soma);
 
 // Subtração
-let subtracao = num1 - num2;
+// let subtracao = num1 - num2;
+// console.log(subtracao);
 
-// Divisão
-let divisao = num1 / num2;
+// // Divisão
+// let divisao = num1 / num2;
+// console.log(divisao)
 
-// Multiplicação
-let multiplicacao = num1 * num2;
+// // Multiplicação
+// let multiplicacao = num1 * num2;
+// console.log(multiplicacao);
 
-// Comparação
-let comparacao = num1 > num2;
+// // Comparação
+// let comparacao = num1 > num2;
+// console.log(comparacao)
--- conditional.js
+++ conditional.js
@@ -1,18 +1,15 @@
-let num1 = 5;
-let num2 = 10;
-let num3 = 15;
-let age = 18;
+let age = 19;
 
-if (age >= 18) {
+if (age > 18) {
   console.log("você pode dirigir");
 } else {
-  console.log("desculpa, você não pode dirigir")
+  console.log("desculpa, você não pode dirigir");
 }
 
-// Como saber se o num1 + num 2 é par?
-let isEven = ((num1 + num2) % 2) == 0
-console.log(isEven);
+// // Como saber se um numero é par?
+// let isEven = (10 % 2) == 0
+// console.log(isEven);
 
-// 1 é igual a "1"
-console.log(1 == "1");
-console.log(1 === "1");
+// // 1 é igual a "1"
+// console.log(1 == "1");
+// console.log(1 === "1");
