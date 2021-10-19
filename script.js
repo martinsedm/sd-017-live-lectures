44839d1661feb2ab6c7c230efea6d48082cd7506
Henrique Jensen <henriquejensen@hotmail.com>
2021-10-19 16:11:36 -0300

    script do dia

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // getElementById
-
+// document.getElementById('month').innerHTML = 'XABLAU !'
 
 // getElementsByTagName
-
+// document.getElementsByTagName('section')[0].style.border = 'solid'
 
 // getElementsByClassName
-
+// document.getElementsByClassName('month-container')[0].style.backgroundColor = 'red'
 
 // querySelector
-
+// document.querySelector('.holiday').style.border = 'solid'
 
 // querySelectorAll
+// document.querySelectorAll('.holiday')[0].style.backgroundColor = 'yellow'
+// document.querySelectorAll('.holiday')[1].style.backgroundColor = 'blue'
+// document.querySelectorAll('.holiday')[2].style.backgroundColor = 'red'
+
+// Mesclas seletores
+// document.querySelector('header ul').style.backgroundColor = 'yellow'
+// document.querySelector('.days-container .holiday').style.backgroundColor = 'blue'
+// document.querySelector('section .week-days').style.backgroundColor = 'red'
+
+// function alteraCor(elemento, cor) {
+//   document.querySelector(elemento).style.backgroundColor = cor;
+// }
+
+// alteraCor('header ul', 'yellow');
+// alteraCor('.days-container .holiday', 'blue');
+// alteraCor('section .week-days', 'red');
